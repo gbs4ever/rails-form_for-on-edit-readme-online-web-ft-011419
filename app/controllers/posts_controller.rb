1a06be0947fb5c34057b91a568118381609cffc1
Sim Greenbaum <gbs4ever2@gmail.com>
2019-03-22 18:21:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,14 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
-
+  def new
+    @post = Post.new
+  end
 	def show
 		@post = Post.find(params[:id])
 	end
 
-	def new
-		@post = Post.new
-	end
+	
 
 	def create
 	  @post = Post.new
